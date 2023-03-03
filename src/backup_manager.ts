b36d89ed5302e241523a4840a06fdeda351c2611
Nikhil Razab-Sekh <nikhilrs@live.com>
2023-03-03 16:41:57 +0100

    listing data works now

--- src/backup_manager.ts
+++ src/backup_manager.ts
@@ -1,44 +1,14 @@
-import AWS from 'aws-sdk';
+    // // Get the VolumeId of the first disk
+    // const volumeId = instance.BlockDeviceMappings?.[0].Ebs?.VolumeId;
 
-// Set the region
-AWS.config.update({region: 'us-east-1'});
+    // // Check if backups are enabled
+    // const backupsEnabled = instance.Tags?.some(tag => tag.Key === 'Backups' && tag.Value === 'Enabled');
 
-// Create an EC2 client object
-const ec2 = new AWS.EC2();
+    // // Get the latest backup time
+    // const backupTime = instance.Tags?.find(tag => tag.Key === 'LatestBackup')?.Value;
 
-// Define parameters for the describeInstances function
-const params: AWS.EC2.Types.DescribeInstancesRequest = {
-  Filters: [
-    {
-      Name: 'instance-state-name',
-      Values: ['running']
-    }
-  ]
-};
-
-// Call the describeInstances function to get a list of running instances
-ec2.describeInstances(params, (err, data) => {
-  if (err) {
-    console.log('Error', err);
-  } else {
-    // Loop through each instance
-    data.Reservations?.forEach(reservation => {
-      reservation.Instances?.forEach(instance => {
-        // Get the VolumeId of the first disk
-        const volumeId = instance.BlockDeviceMappings?.[0].Ebs?.VolumeId;
-
-        // Check if backups are enabled
-        const backupsEnabled = instance.Tags?.some(tag => tag.Key === 'Backups' && tag.Value === 'Enabled');
-
-        // Get the latest backup time
-        const backupTime = instance.Tags?.find(tag => tag.Key === 'LatestBackup')?.Value;
-
-        // Print the instance details
-        console.log(`Instance ID: ${instance.InstanceId}`);
-        console.log(`Volume ID: ${volumeId}`);
-        console.log(`Backups enabled: ${backupsEnabled}`);
-        console.log(`Latest backup time: ${backupTime}`);
-      });
-    });
-  }
-});
+    // // Print the instance details
+    // console.log(`Instance ID: ${instance.InstanceId}`);
+    // console.log(`Volume ID: ${volumeId}`);
+    // console.log(`Backups enabled: ${backupsEnabled}`);
+    // console.log(`Latest backup time: ${backupTime}`);
--- src/index.ts
+++ src/index.ts
@@ -1,45 +1,24 @@
-import { EC2 } from 'aws-sdk';
+import { EC2Client, DescribeInstancesCommand } from "@aws-sdk/client-ec2";
 
-// Set the region
-const region = 'us-east-1';
-const ec2 = new EC2({ region });
-
-// Define parameters for the describeInstances function
-const params: EC2.Types.DescribeInstancesRequest = {
-  Filters: [
-    {
-      Name: 'instance-state-name',
-      Values: ['running']
-    }
-  ]
-};
-
-// Call the describeInstances function to get a list of running instances
-const run = async () => {
-  try {
-    const data = await ec2.describeInstances(params).promise();
-    // Loop through each instance
-    data.Reservations?.forEach(reservation => {
-      reservation.Instances?.forEach(instance => {
+const client = new EC2Client({})
+const command = new DescribeInstancesCommand({});
+const response = client.send(command).then(
+    (data: any) => {data.Reservations?.forEach((reservation: any) => {
+      reservation.Instances?.forEach((instance: any ) => {
         // Get the VolumeId of the first disk
         const volumeId = instance.BlockDeviceMappings?.[0].Ebs?.VolumeId;
 
         // Check if backups are enabled
-        const backupsEnabled = instance.Tags?.some(tag => tag.Key === 'Backups' && tag.Value === 'Enabled');
+        const backupsEnabled = instance.Tags?.some((tag: any) => tag.Key === 'Backups' && tag.Value === 'Enabled');
 
         // Get the latest backup time
-        const backupTime = instance.Tags?.find(tag => tag.Key === 'LatestBackup')?.Value;
-
-        // Print the instance details
+        const backupTime = instance.Tags?.find((tag: any) => tag.Key === 'LatestBackup')?.Value;
+        
         console.log(`Instance ID: ${instance.InstanceId}`);
         console.log(`Volume ID: ${volumeId}`);
         console.log(`Backups enabled: ${backupsEnabled}`);
         console.log(`Latest backup time: ${backupTime}`);
       });
-    });
-  } catch (error) {
-    console.log('Error', error);
-  }
-};
+    });}
+)
 
-run();
